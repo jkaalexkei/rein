0539c0f9ba86bc05cd68c334d4b042c9acf8688b
Ing. Alexander Varela <jkaalexkei@gmail.com>
2022-02-09 09:52:21 -0500

    nuevas configuraciones para hacer el deploy 8

--- appBlog/apps.py
+++ appBlog/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AppblogConfig(AppConfig):
+class appblogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'appblog'
--- appForo/apps.py
+++ appForo/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AppforoConfig(AppConfig):
+class appforoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'appforo'
