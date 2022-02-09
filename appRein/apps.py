3dec7fb390cebf7477f3099da24ce30df8609a50
Ing. Alexander Varela <jkaalexkei@gmail.com>
2022-02-09 09:39:50 -0500

    nuevas configuraciones para hacer el deploy 6

--- appRein/apps.py
+++ appRein/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AppreinConfig(AppConfig):
+class appreinConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apprein'
