222db2f86776f2f6ea6b043348536a1ea8b1f259
Ing. Alexander Varela <jkaalexkei@gmail.com>
2022-02-09 10:36:33 -0500

    nuevas configuraciones para hacer el deploy 13

--- appForo/apps.py
+++ appForo/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AppForoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'appforo'
+    name = 'appForo'
--- appRein/apps.py
+++ appRein/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AppReinConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apprein'
+    name = 'appRein'
