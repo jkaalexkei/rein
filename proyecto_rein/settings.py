58e5ff303dc4a165380a3ec6405dcdf13c73f7b9
Ing. Alexander Varela <jkaalexkei@gmail.com>
2022-02-09 10:31:05 -0500

    nuevas configuraciones para hacer el deploy 11

--- proyecto_rein/settings.py
+++ proyecto_rein/settings.py
@@ -42,9 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apprein',
-    'appblog',
-    'appforo',
+    'AppRein',
+    'AppBlog',
+    'AppForo',
 
 ]
 
